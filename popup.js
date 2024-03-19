1710bb59ec9b8d265152229bc99def033e7c44fd
Alex <glb116@txstate.edu>
2024-03-19 13:29:52 -0500

    Made changes to the popup
    Changed from on and off to a slider with on and off state. Added a range indicator with 5 steps (imagining 1-5 as initial selection range). The range indicator is in an unfinished state, I need to add labels for the range and possibly segments of where the steps occur.

--- popup.js
+++ popup.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var convertButton = document.getElementById('onButton');
-    var resetButton = document.getElementById('offButton');
+  var sliderCheckbox = document.getElementById('slider-1');
 
-    convertButton.addEventListener('click', function() {
+  sliderCheckbox.addEventListener('change', function() {
+    if (this.checked) {
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'on' });
       });
-    });
-
-    resetButton.addEventListener('click', function() {
+    } else {
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'off' });
       });
-    });
+    }
   });
+});
